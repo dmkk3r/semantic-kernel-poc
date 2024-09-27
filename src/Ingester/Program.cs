b2386555073adc7f9d2029b60ced83c7c1f49a8f
dmkk3r <dominik.koerber.95@gmail.com>
2024-09-27 16:55:48 +0200

    Adjusted ingester to support async pipeline

--- src/Ingester/Program.cs
+++ src/Ingester/Program.cs
@@ -1,8 +1,63 @@
 using Ingester;
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.AI.Ollama;
+using Microsoft.KernelMemory.AI.OpenAI;
+using Microsoft.KernelMemory.DocumentStorage.DevTools;
+using Microsoft.KernelMemory.FileSystem.DevTools;
+using Microsoft.KernelMemory.Pipeline.Queue.DevTools;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddLogging();
+builder.Services.AddKernelMemory(kmBuilder =>
+    {
+        var ollamaEndpoint = builder.Configuration["OllamaEndpoint"] ??
+                             throw new ArgumentNullException("Ollama endpoint is not set");
+        var qdrantEndpoint = builder.Configuration["QdrantEndpoint"] ??
+                             throw new ArgumentNullException("Qdrant endpoint is not set");
+        var storagePath = builder.Configuration["StoragePath"] ??
+                          throw new ArgumentNullException("Storage path is not set");
+
+        // TextPartitioningOptions parseOptions = new()
+        // {
+        //     MaxTokensPerParagraph = 300,
+        //     MaxTokensPerLine = 100,
+        //     OverlappingTokens = 50
+        // };
+
+        SimpleFileStorageConfig storageConfig = new()
+        {
+            Directory = storagePath,
+            StorageType = FileSystemTypes.Disk,
+        };
+
+        SimpleQueuesConfig queuesConfig = new()
+        {
+            StorageType = FileSystemTypes.Volatile,
+        };
+
+        OllamaConfig ollamaConfig = new OllamaConfig
+        {
+            Endpoint = ollamaEndpoint,
+            //TextModel = new OllamaModelConfig("llama3.2:3b", 131072),
+            TextModel = new OllamaModelConfig("llama3.1:8b", 131072),
+            EmbeddingModel = new OllamaModelConfig("nomic-embed-text", 2048)
+        };
+
+        QdrantConfig qdrantConfig = new()
+        {
+            Endpoint = qdrantEndpoint,
+        };
+
+        kmBuilder.WithSimpleQueuesPipeline(queuesConfig)
+            .WithSimpleFileStorage(storageConfig)
+            .WithQdrantMemoryDb(qdrantConfig)
+            .WithOllamaTextGeneration(ollamaConfig, new GPT4oTokenizer())
+            .WithOllamaTextEmbeddingGeneration(ollamaConfig, new GPT4oTokenizer());
+            //.With(parseOptions);
+    }
+);
+
 
 builder.Services.AddScoped<TicketIngester>();
 
@@ -12,7 +67,19 @@ app.MapGet("/ingest", async (TicketIngester ticketIngester) =>
 {
     await ticketIngester.IngestAsync();
 
-    return new { };
+    return Results.Ok();
+});
+
+app.MapGet("/ask", async (IKernelMemory kernelMemory) =>
+{
+    var memoryAnswer =
+        await kernelMemory.AskAsync("Wie kann ich einen auftrag in lora anlegen?");
+
+    return Results.Ok(new
+    {
+        Answer = memoryAnswer.Result,
+        Sources = memoryAnswer.RelevantSources
+    });
 });
 
 app.Run();
--- src/Ingester/TicketIngester.cs
+++ src/Ingester/TicketIngester.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.KernelMemory;
 using System.Diagnostics;
-using Microsoft.KernelMemory.DocumentStorage.DevTools;
-using Microsoft.KernelMemory.FileSystem.DevTools;
-using Microsoft.KernelMemory.AI.Ollama;
-using Microsoft.KernelMemory.AI.OpenAI;
 
 namespace Ingester;
 
@@ -11,79 +7,31 @@ public class TicketIngester
 {
     private readonly IConfiguration _configuration;
     private readonly ILogger<TicketIngester> _logger;
+    private readonly IKernelMemory _kernelMemory;
 
-    public TicketIngester(IConfiguration configuration, ILogger<TicketIngester> logger)
+    public TicketIngester(IConfiguration configuration, ILogger<TicketIngester> logger, IKernelMemory kernelMemory)
     {
         _configuration = configuration;
         _logger = logger;
+        _kernelMemory = kernelMemory;
     }
 
     public async Task IngestAsync()
     {
         var ingestPath = _configuration["IngestPath"] ?? throw new ArgumentNullException("Ingest path is not set");
 
-        var memory = CreateKernalMemory(_configuration);
-
-        foreach (var file in Directory.EnumerateFiles(ingestPath, "*"))
+        foreach (var file in Directory.EnumerateFiles(ingestPath, "*", SearchOption.AllDirectories))
         {
-            Stopwatch sw = Stopwatch.StartNew();
+            var sw = Stopwatch.StartNew();
 
             var fileInfo = new FileInfo(file);
 
             _logger.LogInformation("Importing {File}...", fileInfo.Name);
 
-            await memory.ImportDocumentAsync(fileInfo.FullName, fileInfo.Name, steps: Constants.PipelineWithSummary);
+            await _kernelMemory.ImportDocumentAsync(fileInfo.FullName,
+                steps: Constants.PipelineWithoutSummary);
 
             _logger.LogInformation("Imported {File} in {TotalSeconds} seconds", file, sw.Elapsed.TotalSeconds);
         }
     }
-
-    private static IKernelMemory CreateKernalMemory(IConfiguration configuration)
-    {
-        var ollamaEndpoint = configuration["OllamaEndpoint"] ?? throw new ArgumentNullException("Ollama endpoint is not set");
-        var qdrantEndpoint = configuration["QdrantEndpoint"] ?? throw new ArgumentNullException("Qdrant endpoint is not set");
-        var storagePath = configuration["StoragePath"] ?? throw new ArgumentNullException("Storage path is not set");
-
-        //SearchClientConfig searchClientConfig = new()
-        //{
-        //    MaxMatchesCount = 1,
-        //    AnswerTokens = 100,
-        //};
-
-        //TextPartitioningOptions parseOptions = new()
-        //{
-        //    MaxTokensPerParagraph = 50,
-        //    MaxTokensPerLine = 50,
-        //    OverlappingTokens = 40
-        //};
-
-        SimpleFileStorageConfig storageConfig = new()
-        {
-            Directory = storagePath,
-            StorageType = FileSystemTypes.Disk,
-        };
-
-        OllamaConfig ollamaConfig = new OllamaConfig
-        {
-            Endpoint = ollamaEndpoint,
-            TextModel = new OllamaModelConfig("llama3.1:8b", 131072),
-            EmbeddingModel = new OllamaModelConfig("nomic-embed-text", 2048)
-        };
-
-        QdrantConfig qdrantConfig = new()
-        {
-            Endpoint = qdrantEndpoint,
-        };
-
-        return new KernelMemoryBuilder()
-            .WithSimpleFileStorage(storageConfig)
-            .WithQdrantMemoryDb(qdrantConfig)
-            .WithOllamaTextGeneration(ollamaConfig, new GPT4oTokenizer())
-            .WithOllamaTextEmbeddingGeneration(ollamaConfig, new GPT4oTokenizer())
-            //.WithSearchClientConfig(searchClientConfig)
-            //.With(parseOptions)
-            .Build();
-    }
 }
-
-public record Ticket(string Id, string Title, string Description, string Reporter);
